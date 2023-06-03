47ea4351c8de5d955f7c1febf6a428060b8a36bc
Andrii Sozonik <sozonik04.av@gmail.com>
2023-06-03 01:58:15 -0700

    drawing feaute and grids size changing feature

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 
 const grid = document.querySelector('.container')
 
-const range_input = document.querySelector('#grid_size')
-const color = document.querySelector('#color-palette')
+let range_input = document.querySelector('#grid_size')
+let range_input_div = document.querySelector('#rangeinput')
+let color = document.querySelector('#color-palette')
+let grid_size_text = document.querySelector('#grid_size_text')
 
-console.log(color.value)
+function generateGrids() {
 
-function generateGrids(size) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    let size = range_input.value
+
+    grid_size_text.textContent = `Grid size: ${size} x ${size}`
 
     let columns = 'auto'
 
     for (let i = 1; i < size; i++) {
         columns = columns + ' auto'
     }
-    console.log(columns)
+    
 
     grid.style['grid-template-columns'] = columns
 
@@ -22,9 +30,25 @@ function generateGrids(size) {
         //griddy.style['border'] = '0.5px solid black'
         grid.append(griddy);
     }
+
+    let grids = document.querySelectorAll('.container div')
+
+    for (let i = 0; i < grids.length; i++) {
+        grids[i].addEventListener('mousemove', function(e) {
+            this.style['background-color'] = color.value
+        })
+    }
 }
 
-generateGrids(range_input.value)
+range_input.addEventListener('change', function() {
+    generateGrids()
+})
+
+generateGrids()
+
+
+
+
 
 
 
